806a84f1338a560bae464f287468ae42884f2c6e
Fernando Gaol <ffilho@riolindo.org>
2019-11-10 17:52:00 -0700

    new landing page added

--- burgerMenu.js
+++
@@ -1,8 +0,0 @@
-function burgerMenu() {
-  let x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
