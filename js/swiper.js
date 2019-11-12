c5521243913ea348ec733c6d7000071e644a3fc8
Fernando Gaol <ffilho@riolindo.org>
2019-11-11 20:04:32 -0700

    organized files in folders

---
+++ js/swiper.js
@@ -0,0 +1,16 @@
+function getSwiper() {
+  let swiper = new Swiper(".swiper-container", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+      el: ".swiper-pagination"
+    },
+    mousewheel: true,
+    keyboard: true
+  });
+}
+
+getSwiper();
