83acdce1d930e7181cdcca99912ed95b3348e224
Fernando Gaol <ffilho@riolindo.org>
2020-04-24 13:35:38 -0600

    made it mobile first

---
+++ index.js
@@ -0,0 +1,12 @@
+$(".menu").on("click", event => {
+  $(".home").scroll();
+});
+// $(".about").on("click", event => {
+//   location.reload(false);
+// });
+// $(".projects").on("click", event => {
+//   location.reload(false);
+// });
+// $(".contact").on("click", event => {
+//   location.reload(false);
+// });
